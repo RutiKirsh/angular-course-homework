8b1b2eee2b04f5859d3bcf3acc823a22b8a92ad6
RutiKirsh <ruti4293@gmail.com>
2025-12-30 13:03:02 +0200

    h.w. lesson 7

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,14 +7,18 @@ import { FormsModule } from '@angular/forms';
 // import { Lesson5 } from './lesson5/lesson5';
 // import { pipes } from './pipes/pipes';
 import { ListNumbers } from './list-numbers/list-numbers';
+import { User } from './user/user';
+import { Details } from './details/details';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, /*MyDetails, Student Person, Lesson5, ...pipes,*/ ListNumbers],
+  imports: [CommonModule, FormsModule, /*MyDetails, Student Person, Lesson5, ...pipes,ListNumbers,*/  User, Details],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  flag: boolean = false;
   protected readonly title = signal('lesson1');
+
 }
